342d42973a784a52664acf12e9b3d14d31668ec0
jaisharma06 <jai.sharma06@live.com>
2018-01-21 22:24:46 +0530

    Initial commit

---
+++ Assets/Scripts/Statics.cs
@@ -0,0 +1,6 @@
+﻿public static class Statics
+{
+    public static string PLAY_BUTTON = "Play";
+    public static string PAUSE_BUTTON = "Pause";
+    public static string RESTART_BUTTON = "Restart";
+}
---
+++ Assets/Scripts/VideoPlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+[RequireComponent(typeof(VideoPlayer))]
+public class VideoPlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private VideoPlayer videoPlayer;
+
+    private void Start()
+    {
+        if (!videoPlayer)
+        {
+            videoPlayer = GetComponent<VideoPlayer>();
+        }
+    }
+
+    private void Update()
+    {
+        ControlVideo();
+    }
+
+    private void ControlVideo()
+    {
+        if (Input.GetButtonUp(Statics.PLAY_BUTTON))
+        {
+            HandlePlayBack();
+        }
+        else if (Input.GetButtonUp(Statics.RESTART_BUTTON))
+        {
+            videoPlayer.Stop();
+            videoPlayer.Play();
+        }
+    }
+
+    public void HandlePlayBack()
+    {
+        if (videoPlayer.isPlaying)
+        {
+            videoPlayer.Pause();
+        }
+        else
+        {
+            videoPlayer.Play();
+        }
+    }
+}
